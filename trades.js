06884099e1b08dfc7702933b025011b9b23eacdb
gerard hemgren <gerardhemgren@gmail.com>
2022-04-08 12:32:17 -0300

    first commit

---
+++ trades.js
@@ -0,0 +1,54 @@
+let tradesStore = [
+    {
+        name: 'USD',
+        quant: 10,
+        id: '11'
+    },
+    {
+        name: 'EUR',
+        quant: 10,
+        id: '54'
+    },
+    {
+        name: 'ARS',
+        quant: 10,
+        id: '24'
+    }, {
+        name: 'JPY',
+        quant: 10,
+        id: '34'
+    },
+    {
+        name: 'CHF',
+        quant: 10,
+        id: '5a4'
+    },
+    {
+        name: 'SEK',
+        quant: 10,
+        id: '2d4'
+    }, {
+        name: 'QAR',
+        quant: 10,
+        id: '3s4'
+    }, {
+        name: 'LUF',
+        quant: 10,
+        id: '344'
+    },
+    {
+        name: 'AUD',
+        quant: 10,
+        id: '5as4'
+    },
+    {
+        name: 'GBP',
+        quant: 10,
+        id: '2da4'
+    }, 
+    {
+        name: 'BRL',
+        quant: 10,
+        id: '3sa4'
+    }
+];
