278bb2040890212656d37749e50ddb5575f2641b
gerard hemgren <gerardhemgren@gmail.com>
2022-04-12 10:45:42 -0300

    .gitignore removed

---
+++ config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAzvLi2uL-krAig2v3rr-ZDzW7YVz_EXSY",
+    authDomain: "gutentrades.firebaseapp.com",
+    projectId: "gutentrades",
+    storageBucket: "gutentrades.appspot.com",
+    messagingSenderId: "863068269901",
+    appId: "1:863068269901:web:69ef81e086007ef38edb04"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
